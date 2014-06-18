5353090228f9f3f6166fd20235f8ba4fdb15b8c7
fabricebrito <fabrice.brito@terradue.com>
2014-06-18 07:43:48 +0200

    first submission

--- src/main/app-resources/py-ndvi/run.py
+++ src/main/app-resources/py-ndvi/run.py
@@ -18,9 +18,9 @@ import cioppy as ciop
 
 ciop.log('INFO', 'Hello World')
 
-myvar = ciop.getparam('param1')
+#myvar = ciop.getparam('param1')
 
-ciop.log('DEBUG', 'value is: ' + myvar)
+#ciop.log('DEBUG', 'value is: ' + myvar)
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
@@ -41,11 +41,17 @@ for line in sys.stdin:
                 "dc:identifier=mydataset",
                 "dct:spatial=MULTIPOLYGON(((25.55215 36.97701,24.740512 37.091395,24.496927 35.950137,25.284346 35.839142,25.55215 36.97701)))",
                 "dclite4g:onlineResource=" + pub[0].rstrip()]
-    
+
+    metadata = [ "ical:dtstart=2001-01-10T14:00:00",
+                "ical:dtend=2001-01-10T14:05:00",
+                "dc:identifier=mydataset",
+                "dct:spatial=MULTIPOLYGON(((25.55215 36.97701,24.740512 37.091395,24.496927 35.950137,25.284346 35.839142,25.55215 36.97701)))",
+                "dclite4g:onlineResource=http://some.host.com/myproduct.tif"]   
+ 
     ciop.log('DEBUG', 'Going to register')
 
     ciop.register('http://localhost/catalogue/sandbox/rdf',
-                    'file:///application/pyjob/etc/series.rdf',
+                    'file:///application/py-ndvi/etc/series.rdf',
                     metadata)
 
     ciop.publish('/tmp/pippo.tif', metalink = True)
